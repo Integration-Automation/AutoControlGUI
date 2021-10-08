d7cf388e150e3a41826c2a6d84ea9bd0b71979c9
JE-Chen <zenxcvwait@gmail.com>
2021-10-08 11:01:03 -0700

    add osx mouse hook
    add osx mouse hook
    edit osx keyboard hook event

--- je_auto_control/osx/listener/osx_mouse_listener.py
+++ je_auto_control/osx/listener/osx_mouse_listener.py
@@ -5,11 +5,14 @@ from PyObjCTools import AppHelper
 
 class AppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, aNotification):
-        NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSEventMaskKeyDown, handler)
+        NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSEventMaskLeftMouseDown, left_handler)
+        NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSEventMaskRightMouseDown, right_handler)
 
-def handler(event):
+def left_handler(event):
     print(event)
-    print(str(hex(event.keyCode())))
+
+def right_handler(event):
+    print(event)
 
 app = NSApplication.sharedApplication()
 delegate = AppDelegate.alloc().init()
