0066b14cdbbed878944d449a96b9d81bc15c4208
JE-Chen <zenxcvwait@gmail.com>
2021-10-09 04:01:47 +0800

    refactor win32 listener
    add record and stop_record method
    add thread

--- je_auto_control/windows/listener/win32_keyboard_listener.py
+++ je_auto_control/windows/listener/win32_keyboard_listener.py
@@ -1,5 +1,14 @@
+import sys
+
+if sys.platform not in ["win32", "cygwin", "msys"]:
+    raise Exception("should be only loaded on windows")
+
 from ctypes import *
 from ctypes.wintypes import MSG
+
+from threading import Thread
+
+from queue import Queue
 
 user32 = windll.user32
 kernel32 = windll.kernel32
@@ -7,13 +16,16 @@ kernel32 = windll.kernel32
 wm_keydown = 0x100
 
 
-class Win32Listener:
+class Win32KeyboardListener(Thread):
 
-    def __init__(self, hook_event_code_int):
+    def __init__(self):
+        super().__init__()
         self.hooked = None
-        self.hook_event_code_int = hook_event_code_int
+        self.record_queue = None
+        self.record_flag = False
+        self.hook_event_code_int = 13
 
-    def set_win32_hook(self, point):
+    def _set_win32_hook(self, point):
         self.hooked = user32.SetWindowsHookExA(
             self.hook_event_code_int,
             point,
@@ -24,36 +36,54 @@ class Win32Listener:
             return False
         return True
 
-    def remove_win32_hook_proc(self):
+    def _remove_win32_hook_proc(self):
         if self.hooked is None:
             return
         user32.UnhookWindowsHookEx(self.hooked)
         self.hooked = None
+        self.record_queue = None
+        sys.exit(0)
 
-    def win32_hook_proc(self, code, w_param, l_param):
+    def _win32_hook_proc(self, code, w_param, l_param):
         if w_param is not wm_keydown:
             return user32.CallNextHookEx(self.hooked, code, w_param, l_param)
+        if self.record_flag is True:
             # int to hex
-        temp = hex(l_param[0] & 0xFFFFFFFF)
-        print("Hooked Key: " + temp)
-        print(int(temp, 16))
+            temp = hex(l_param[0] & 0xFFFFFFFF)
+            self.record_queue.put(("test_keyboard", temp))
         return user32.CallNextHookEx(self.hooked, code, w_param, l_param)
 
-    def get_function_pointer(self, function):
+    def _get_function_pointer(self, function):
         win_function = WINFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
         return win_function(function)
 
-    def start_listener(self):
-        pointer = self.get_function_pointer(self.win32_hook_proc)
-        if self.set_win32_hook(pointer):
+    def _start_listener(self):
+        pointer = self._get_function_pointer(self._win32_hook_proc)
+        if self._set_win32_hook(pointer):
             print("start listener")
         else:
             print("failed to start")
         message = MSG()
         user32.GetMessageA(byref(message), 0, 0, 0)
 
+    def record(self):
+        self.record_flag = True
+        self.record_queue = Queue()
+        self.start()
+
+    def stop_record(self):
+        self.record_flag = False
+        return self.record_queue
+
+    def run(self):
+        self._start_listener()
+
 
 if __name__ == "__main__":
-    wh_keyboard_ll = 13
-    win32_listener = Win32Listener(wh_keyboard_ll)
-    win32_listener.start_listener()
+    win32_keyboard_listener = Win32KeyboardListener()
+    win32_keyboard_listener.record()
+    from time import sleep
+    sleep(3)
+    temp = win32_keyboard_listener.stop_record()
+    for i in temp.queue:
+        print(i)
--- je_auto_control/windows/listener/win32_mouse_listener.py
+++ je_auto_control/windows/listener/win32_mouse_listener.py
@@ -1,21 +1,37 @@
+import sys
+
+if sys.platform not in ["win32", "cygwin", "msys"]:
+    raise Exception("should be only loaded on windows")
+
 from ctypes import *
 from ctypes.wintypes import MSG
+
+from threading import Thread
+
+from queue import Queue
 
 from je_auto_control.windows.mouse.win32_ctype_mouse_control import position
 
 user32 = windll.user32
 kernel32 = windll.kernel32
+"""
+Left mouse button down 0x0201
+Right mouse button down 0x0204
+Middle mouse button down 0x0207
+"""
+wm_mouse_key_code = [0x0201, 0x0204, 0x0207]
 
-wm_mouse_key_code = [0x0201, 0x0204]
 
+class Win32MouseListener(Thread):
 
-class Win32Listener:
-
-    def __init__(self, hook_event_code_int):
+    def __init__(self):
+        super().__init__()
         self.hooked = None
-        self.hook_event_code_int = hook_event_code_int
+        self.record_queue = None
+        self.record_flag = False
+        self.hook_event_code_int = 14
 
-    def set_win32_hook(self, point):
+    def _set_win32_hook(self, point):
         self.hooked = user32.SetWindowsHookExA(
             self.hook_event_code_int,
             point,
@@ -26,33 +42,56 @@ class Win32Listener:
             return False
         return True
 
-    def remove_win32_hook_proc(self):
+    def _remove_win32_hook_proc(self):
         if self.hooked is None:
             return
         user32.UnhookWindowsHookEx(self.hooked)
         self.hooked = None
+        self.record_queue = None
+        sys.exit(0)
 
-    def win32_hook_proc(self, code, w_param, l_param):
+    def _win32_hook_proc(self, code, w_param, l_param):
         if w_param not in wm_mouse_key_code:
             return user32.CallNextHookEx(self.hooked, code, w_param, l_param)
-        print(position())
+        if w_param == wm_mouse_key_code[0] and self.record_flag is True:
+            self.record_queue.put(("test_left", position()))
+        elif w_param == wm_mouse_key_code[1] and self.record_flag is True:
+            self.record_queue.put(("test_right", position()))
+        elif w_param == wm_mouse_key_code[2] and self.record_flag is True:
+            self.record_queue.put(("test_middle", position()))
         return user32.CallNextHookEx(self.hooked, code, w_param, l_param)
 
-    def get_function_pointer(self, function):
+    def _get_function_pointer(self, function):
         win_function = WINFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
         return win_function(function)
 
-    def start_listener(self):
-        pointer = self.get_function_pointer(self.win32_hook_proc)
-        if self.set_win32_hook(pointer):
+    def _start_listener(self):
+        pointer = self._get_function_pointer(self._win32_hook_proc)
+        if self._set_win32_hook(pointer):
             print("start listener")
         else:
             print("failed to start")
         message = MSG()
         user32.GetMessageA(byref(message), 0, 0, 0)
 
+    def record(self):
+        self.record_flag = True
+        self.record_queue = Queue()
+        self.start()
+
+    def stop_record(self):
+        self.record_flag = False
+        return self.record_queue
+
+    def run(self):
+        self._start_listener()
+
 
 if __name__ == "__main__":
-    wh_mouse_ll = 14
-    win32_listener = Win32Listener(wh_mouse_ll)
-    win32_listener.start_listener()
+    win32_mouse_listener = Win32MouseListener()
+    win32_mouse_listener.record()
+    from time import sleep
+    sleep(3)
+    temp = win32_mouse_listener.stop_record()
+    for i in temp.queue:
+        print(i)
