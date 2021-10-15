0d5a03399cfafde9f41ad486bf6baa60e60a1e24
JE-Chen <zenxcvwait@gmail.com>
2021-10-16 00:12:54 +0800

    fix test error
    error by keycode

--- circle_ci_test/execute_action_test.py
+++ circle_ci_test/execute_action_test.py
@@ -6,7 +6,7 @@ test_list = None
 if sys.platform in ["win32", "cygwin", "msys"]:
     test_list = [("keyboard", 65), ("keyboard", 0x41), ("keyboard", 0x41), ("keyboard", 0x41)]
 elif sys.platform in ["linux", "linux2"]:
-    test_list = [("keyboard", 0x00), ("keyboard", 0x00), ("keyboard", 0x00), ("keyboard", 0x00)]
-elif sys.platform in ["darwin"]:
     test_list = [("keyboard", 38), ("keyboard", 38), ("keyboard", 38), ("keyboard", 38)]
+elif sys.platform in ["darwin"]:
+    test_list = [("keyboard", 0x00), ("keyboard", 0x00), ("keyboard", 0x00), ("keyboard", 0x00)]
 execute_action(test_list)
