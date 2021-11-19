072eb44f1fe3adc49ebd6f94e8efa38b5bdeeef1
JE-Chen <zenxcvwait@gmail.com>
2021-11-19 14:11:29 +0800

    refactor action executor
    refactor action executor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_auto_control",
-    version="0.0.69",
+    version="0.0.71",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="auto testing",
