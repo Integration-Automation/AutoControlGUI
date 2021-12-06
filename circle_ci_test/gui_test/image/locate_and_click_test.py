af241c96b4c1b708e282db1980c532fb49670004
JE-Chen <zenxcvwait@gmail.com>
2021-12-07 01:41:20 +0800

    Update locate_and_click_test.py
    fix test

--- circle_ci_test/gui_test/image/locate_and_click_test.py
+++ circle_ci_test/gui_test/image/locate_and_click_test.py
@@ -7,5 +7,5 @@ time.sleep(2)
 detect_threshold 0~1 , 1 is absolute equal
 draw_image, mark the find target
 """
-image_data = locate_and_click(screenshot(), detect_threshold=0.9, draw_image=False)
+image_data = locate_and_click(screenshot(), "mouse_left", detect_threshold=0.9, draw_image=False)
 print(image_data)
