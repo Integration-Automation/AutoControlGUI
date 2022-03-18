202ce8a64a695929840e0a0a65c337ec0705ea92
JE-Chen <zenxcvwait@gmail.com>
2022-03-18 17:03:57 +0800

    fix test
    fix test

--- test/unit_test/critical_exit/real_critical_test.py
+++ test/unit_test/critical_exit/real_critical_test.py
@@ -1,25 +1,26 @@
 import sys
-from time import sleep
 
 from je_auto_control import set_position
 from je_auto_control import size
 from je_auto_control import CriticalExit
 from je_auto_control import press_key
-from je_auto_control import AutoControlMouseException
 
-"""
-print your screen width and height
-"""
-print(size())
-
-"""
-simulate you can't use your mouse because you use while true to set mouse position
-"""
-CriticalExit().init_critical_exit()
 try:
-    while True:
-        set_position(200, 400)
-        set_position(400, 600)
-        press_key("f7")
+    """
+    print your screen width and height
+    """
+    print(size())
+
+    """
+    simulate you can't use your mouse because you use while true to set mouse position
+    """
+    CriticalExit().init_critical_exit()
+    try:
+        while True:
+            set_position(200, 400)
+            set_position(400, 600)
+            press_key("f7")
+    except Exception as error:
+        print(repr(error), file=sys.stderr)
 except Exception as error:
     print(repr(error), file=sys.stderr)
