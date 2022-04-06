b421290d275dfd25c8b42ecdea0229ec258f37c0
JE-Chen <zenxcvwait@gmail.com>
2022-04-06 10:08:26 +0800

    refactor __main__
    refactor __main__

--- test/unit_test/argparse/argparse_test.py
+++ test/unit_test/argparse/argparse_test.py
@@ -3,6 +3,6 @@ import os
 print(os.getcwd())
 
 os.system("cd " + os.getcwd())
-os.system("python je_auto_control " + os.getcwd() + r"/test/unit_test/argparse/test.json")
+os.system("python je_auto_control --execute_file " + os.getcwd() + r"/test/unit_test/argparse/test.json")
 
 
