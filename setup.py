7b64f658899c2fad2f93a2ed82b6c4d0edc05f68
JE-Chen <zenxcvwait@gmail.com>
2022-04-12 20:00:16 +0800

    update stable version
    update stable version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_auto_control",
-    version="0.0.86",
+    version="0.0.87",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="auto testing",
