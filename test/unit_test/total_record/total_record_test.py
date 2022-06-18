1e72da7ca46ff6d8b532eb7425c0535ecca923ac
JE-Chen <zenxcvwait@gmail.com>
2022-06-18 08:00:15 +0800

    test

--- test/unit_test/total_record/total_record_test.py
+++ test/unit_test/total_record/total_record_test.py
@@ -1,16 +1,10 @@
 import sys
 
-print("t1")
 from je_auto_control import keys_table
-print("t1")
 from je_auto_control import press_key
-print("t1")
 from je_auto_control import release_key
-print("t1")
 from je_auto_control import test_record_instance
-print("t1")
 from je_auto_control import write
-print("t1")
 
 try:
     test_record_instance.set_record_enable(True)
@@ -22,14 +16,14 @@ try:
     release_key("shift")
     press_key("return")
     release_key("return")
-    print(write("abcdefghijklmnopqrstuvwxyz") == "abcdefghijklmnopqrstuvwxyz")
+    assert (write("abcdefghijklmnopqrstuvwxyz") == "abcdefghijklmnopqrstuvwxyz")
     press_key("return")
     release_key("return")
     """
     this write will print one error -> keyboard write error can't find key : Ѓ and write remain string
     """
     try:
-        print(write("?123456789") == "123456789")
+        assert (write("?123456789") == "123456789")
     except Exception as error:
         print(repr(error), file=sys.stderr)
     try:
