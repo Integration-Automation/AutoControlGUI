8e42dfb27175cbb6096c8c56e18ec899645243fc
JE-Chen <zenxcvwait@gmail.com>
2022-06-20 19:14:45 +0800

    test
    test

--- test/unit_test/keyboard/keyboard_write_test.py
+++ test/unit_test/keyboard/keyboard_write_test.py
@@ -3,6 +3,7 @@ import sys
 from je_auto_control import keys_table
 from je_auto_control import press_key
 from je_auto_control import release_key
+from je_auto_control import type_key
 from je_auto_control import write
 
 print(keys_table.keys())
@@ -24,3 +25,9 @@ try:
     write("!#@L@#{@#PL#{!@#L{!#{|##PO}!@#O@!O#P!)KI#O_!K")
 except Exception as error:
     print(repr(error), file=sys.stderr)
+if sys.platform in ["win32", "cygwin"]:
+    for i in range(61):
+        type_key("back")
+else:
+    for i in range(61):
+        type_key("backspace")
