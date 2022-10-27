0a0a2bc5d7989c8dc901c67ce9aaf62f555bc802
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-10-27 14:12:21 +0800

    add driver and upload dev
    add driver and upload dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_auto_control",
-    version="0.0.105",
+    version="0.0.106",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="auto testing",
