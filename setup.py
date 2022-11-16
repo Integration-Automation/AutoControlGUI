65b714cad9b8f008d1004327e2b5835b3a9ca87e
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-11-16 09:34:55 +0800

    update stable and dev and test
    update stable and dev and test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="je_auto_control",
-    version="0.0.111",
+    version="0.0.112",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="auto testing",
