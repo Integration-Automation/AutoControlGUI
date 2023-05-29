cf95a23ebaa41b8739ecf06c9e3c9d4575d5e784
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-05-29 11:25:49 +0800

    Update dev version
    Update dev version

--- je_auto_control/utils/executor/action_executor.py
+++ je_auto_control/utils/executor/action_executor.py
@@ -8,16 +8,87 @@ from je_auto_control.utils.exception.exception_tags import action_is_null_error,
 from je_auto_control.utils.exception.exception_tags import cant_execute_action_error
 from je_auto_control.utils.exception.exceptions import AutoControlActionException, AutoControlAddCommandException
 from je_auto_control.utils.exception.exceptions import AutoControlActionNullException
-from je_auto_control.utils.global_dict.event_dict import event_dict
+from je_auto_control.utils.generate_report.generate_html_report import generate_html
+from je_auto_control.utils.generate_report.generate_html_report import generate_html_report
+from je_auto_control.utils.generate_report.generate_json_report import generate_json
+from je_auto_control.utils.generate_report.generate_json_report import generate_json_report
+from je_auto_control.utils.generate_report.generate_xml_report import generate_xml
+from je_auto_control.utils.generate_report.generate_xml_report import generate_xml_report
 from je_auto_control.utils.json.json_file import read_action_json
 from je_auto_control.utils.package_manager.package_manager_class import package_manager
-from je_auto_control.utils.test_record.record_test_class import record_action_to_list
+from je_auto_control.utils.project.create_project_structure import create_project_dir
+from je_auto_control.utils.shell_process.shell_exec import ShellManager
+from je_auto_control.utils.start_exe.start_another_process import start_exe
+from je_auto_control.utils.test_record.record_test_class import record_action_to_list, test_record_instance
+from je_auto_control.wrapper.auto_control_image import locate_all_image, locate_and_click, locate_image_center
+from je_auto_control.wrapper.auto_control_keyboard import check_key_is_press
+from je_auto_control.wrapper.auto_control_keyboard import get_special_table, get_keyboard_keys_table
+from je_auto_control.wrapper.auto_control_keyboard import press_keyboard_key, release_keyboard_key, hotkey, \
+    type_keyboard, write
+from je_auto_control.wrapper.auto_control_mouse import get_mouse_position, press_mouse, release_mouse, click_mouse, \
+    mouse_scroll
+from je_auto_control.wrapper.auto_control_mouse import get_mouse_table
+from je_auto_control.wrapper.auto_control_mouse import set_mouse_position
+from je_auto_control.wrapper.auto_control_record import record, stop_record
+from je_auto_control.wrapper.auto_control_screen import screenshot, screen_size
 
 
 class Executor(object):
 
     def __init__(self):
-        self.event_dict: dict = event_dict
+        self.event_dict: dict = {
+            # mouse
+            "mouse_left": click_mouse,
+            "mouse_right": click_mouse,
+            "mouse_middle": click_mouse,
+            "click_mouse": click_mouse,
+            "get_mouse_table": get_mouse_table,
+            "get_mouse_position": get_mouse_position,
+            "press_mouse": press_mouse,
+            "release_mouse": release_mouse,
+            "mouse_scroll": mouse_scroll,
+            "set_mouse_position": set_mouse_position,
+            "get_special_table": get_special_table,
+            # keyboard
+            "get_keyboard_keys_table": get_keyboard_keys_table,
+            "type_keyboard": type_keyboard,
+            "press_keyboard_key": press_keyboard_key,
+            "release_keyboard_key": release_keyboard_key,
+            "check_key_is_press": check_key_is_press,
+            "write": write,
+            "hotkey": hotkey,
+            # image
+            "locate_all_image": locate_all_image,
+            "locate_image_center": locate_image_center,
+            "locate_and_click": locate_and_click,
+            # screen
+            "screen_size": screen_size,
+            "screenshot": screenshot,
+            # test record
+            "set_record_enable": test_record_instance.set_record_enable,
+            # only generate
+            "generate_html": generate_html,
+            "generate_json": generate_json,
+            "generate_xml": generate_xml,
+            # generate report
+            "generate_html_report": generate_html_report,
+            "generate_json_report": generate_json_report,
+            "generate_xml_report": generate_xml_report,
+            # record
+            "record": record,
+            "stop_record": stop_record,
+            # execute
+            "execute_action": self.execute_action,
+            "execute_files": self.execute_files,
+            "add_package_to_executor": package_manager.add_package_to_executor,
+            "add_package_to_callback_executor": package_manager.add_package_to_callback_executor,
+            # project
+            "create_project": create_project_dir,
+            # Shell
+            "shell_command": ShellManager().exec_shell,
+            # Another process
+            "execute_process": start_exe,
+        }
         # get all builtin function and add to event dict
         for function in getmembers(builtins, isbuiltin):
             self.event_dict.update({str(function[0]): function[1]})
