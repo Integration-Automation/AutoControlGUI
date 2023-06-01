72f94746f9bb93b85ace157c7edeba41c22711d1
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-06-01 10:05:40 +0800

    Add scheduler and tests
    Add scheduler and tests

---
+++ test/unit_test/scheduler_test/sec_interval_test.py
@@ -0,0 +1,12 @@
+from je_auto_control import SchedulerManager
+
+
+def test_scheduler():
+    print("Test Scheduler")
+    scheduler.remove_blocking_job(id="test")
+    scheduler.shutdown_blocking_scheduler()
+
+
+scheduler = SchedulerManager()
+scheduler.add_interval_blocking_secondly(function=test_scheduler, id="test")
+scheduler.start_block_scheduler()
