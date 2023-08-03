6e527e3cf802c7773e4c80302dcdf6d615125e6a
JeffreyChen <zenmailman@gmail.com>
2023-08-03 16:25:40 +0800

    Update dev and stable version
    Update dev and stable version
    * Now new log file will replace old log not append

--- je_auto_control/utils/logging/loggin_instance.py
+++ je_auto_control/utils/logging/loggin_instance.py
@@ -10,6 +10,6 @@ stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.WARNING)
 auto_control_logger.addHandler(stream_handler)
 # File handler
-file_handler = logging.FileHandler("AutoControl.log")
+file_handler = logging.FileHandler(filename="AutoControl.log", mode="w")
 file_handler.setFormatter(formatter)
 auto_control_logger.addHandler(file_handler)
