4eedb7b4dd2523860774806fabbc8132505b9584
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-08-14 10:19:19 +0800

    Update dev and stable version

---
+++ test/gui_test/calculator/calculator_executor.py
@@ -0,0 +1,69 @@
+import subprocess
+from time import sleep
+
+from je_auto_control import locate_and_click, executor
+
+# 開啟windows 計算機
+# 並累加1至9
+# open windows calc.exe
+# and calculate 1 + 2 .... + 9
+
+test_list = [
+    ["AC_add_package_to_executor", {"package": "subprocess"}],
+    ["subprocess_Popen", {"args": "calc"}],
+    ["AC_add_package_to_executor", {"package": "time"}],
+    ["time_sleep", [3]],
+    ["AC_locate_and_click",
+     {"image": "./test_source/1.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/2.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/3.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/4.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/5.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/6.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/7.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/8.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/plus.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/9.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}],
+    ["AC_locate_and_click",
+     {"image": "./test_source/equal.png", "mouse_keycode": "mouse_left", "detect_threshold": 0.9}]
+]
+
+executor.execute_action(test_list)
+
--- test/unit_test/execute_action/execute_action_test.py
+++ test/unit_test/execute_action/execute_action_test.py
@@ -8,29 +8,29 @@ test_record_instance.init_record = True
 if sys.platform in ["win32", "cygwin", "msys"]:
     test_list = [
         ["AC_type_keyboard", {"keycode": 65}],
-        ["AC_mouse_left", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
+        ["AC_mouse_left", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
         ["AC_get_mouse_position"],
-        ["AC_press_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
-        ["AC_release_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
-        ["AC_type_keyboard", {"mouse_keycode": "dwadwawda", "dwadwad": 500, "wdawddwawad": 500}]
+        ["AC_press_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
+        ["AC_release_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
+        ["AC_type_keyboard", {"keycode": "dwadwawda", "dwadwad": 500, "wdawddwawad": 500}]
     ]
 
 elif sys.platform in ["linux", "linux2"]:
     test_list = [
         ["AC_type_keyboard", {"keycode": 38}],
-        ["AC_mouse_left", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
+        ["AC_mouse_left", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
         ["AC_get_mouse_position"],
-        ["AC_press_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
-        ["AC_release_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
+        ["AC_press_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
+        ["AC_release_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
         ["AC_type_keyboard", {"mouse_keycode": "dwadwawda", "dwadwad": 500, "wdawddwawad": 500}]
     ]
 elif sys.platform in ["darwin"]:
     test_list = [
         ["AC_type_keyboard", {"keycode": 0x00}],
-        ["AC_mouse_left", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
+        ["AC_mouse_left", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
         ["AC_get_mouse_position"],
-        ["AC_press_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
-        ["AC_release_mouse", {"mouse_keycode": "AC_mouse_left", "x": 500, "y": 500}],
+        ["AC_press_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
+        ["AC_release_mouse", {"mouse_keycode": "mouse_left", "x": 500, "y": 500}],
         ["AC_type_keyboard", {"mouse_keycode": "dwadwawda", "dwadwad": 500, "wdawddwawad": 500}]
     ]
 print("\n\n")
