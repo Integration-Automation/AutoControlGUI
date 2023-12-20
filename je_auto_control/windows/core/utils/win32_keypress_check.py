8369e584ca02335150aeef069243d37a3f88d3fe
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-12-20 18:54:02 +0800

    Add post and send message to window
    Add post and send message to window

--- je_auto_control/windows/core/utils/win32_keypress_check.py
+++ je_auto_control/windows/core/utils/win32_keypress_check.py
@@ -11,9 +11,9 @@ import ctypes
 
 def check_key_is_press(keycode: [int, str]) -> bool:
     if isinstance(keycode, int):
-        temp: int = ctypes.windll.user32.GetKeyState(keycode)
+        temp: int = ctypes.windll.user32.GetAsyncKeyState(keycode)
     else:
-        temp = ctypes.windll.user32.GetKeyState(ord(keycode))
+        temp = ctypes.windll.user32.GetAsyncKeyState(ord(keycode))
     if temp > 1:
         return True
     return False
